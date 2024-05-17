32163dc5f27e9366d9170bcd61adbba65162e463
Jit <connects2jit@gmail.com>
2024-05-17 06:50:10 +0530

    initial empty project push

---
+++ src/main/java/jit/com/example/OperationsOnFiles/OperationsOnFilesApplication.java
@@ -0,0 +1,13 @@
+package jit.com.example.OperationsOnFiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationsOnFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationsOnFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jit/com/example/OperationsOnFiles/OperationsOnFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package jit.com.example.OperationsOnFiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationsOnFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
